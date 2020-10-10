1e7b2a19beebb7642018a355e82e857030a84dc9
Adam Paul <adammpaul4@gmail.com>
2020-10-10 03:22:47 -0400

    Initial commit

---
+++ src/main/java/Controller.java
@@ -0,0 +1,22 @@
+import javafx.fxml.FXML;
+
+import javafx.scene.control.Label;
+
+
+public class Controller {
+
+
+  @FXML
+
+  private Label lblOutput;
+
+
+  public void sayHello() {
+
+    lblOutput.setText("Hello FXML!");
+
+
+  }
+
+
+}
---
+++ src/main/java/Main.java
@@ -0,0 +1,38 @@
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+
+public class Main extends Application {
+
+  /**
+   * Main method for program.
+   *
+   * @param args -- Command line arguments
+   */
+  public static void main(String[] args) {
+    launch(args);
+  }
+
+  /**
+   * Initializes JavaFX GUI window.
+   *
+   * @param primaryStage - Primary stage for application
+   */
+  @Override
+  public void start(Stage primaryStage) throws Exception {
+
+    Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+
+    Scene scene = new Scene(root, 300, 275);
+
+    primaryStage.setTitle("Encryption Project");
+
+    primaryStage.setScene(scene);
+
+    primaryStage.show();
+
+  }
+}
