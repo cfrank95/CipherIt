a96648a5622e13d98dc6ae7bc331aaed35f3b273
Joseph Morelli <jgmorelli5207@eagle.fgcu.edu>
2020-10-10 04:06:58 -0400

    GUI Testing

--- src/main/java/Controller.java
+++ src/main/java/Controller.java
@@ -1,9 +1,15 @@
+import java.util.Scanner;
 import javafx.fxml.FXML;
 
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
 
 
 public class Controller {
+
+  static Scanner keyboard = new Scanner(System.in);
+  public TextField messageString;
 
 
   @FXML
@@ -18,5 +24,89 @@ public class Controller {
 
   }
 
+  public static StringBuilder atbashCiphering(String plainString) {
 
+    String alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    String alphaLower = "abcdefghijklmnopqrstuvwxyz";
+
+    int strLength = plainString.length();
+    int alphLength = alphabet.length();
+    StringBuilder cipher = new StringBuilder("");
+
+    for (int i = 0; i < strLength; i++) {
+      char b = plainString.charAt(i);
+      if (b == ' ') {
+        cipher.append(" ");
+      }
+
+      for (int j = 0; j < alphLength; j++) {
+        char c = alphabet.charAt(j);
+        char d = alphaLower.charAt(j);
+
+        if (c == b) {
+          int index = alphabet.indexOf(c);
+          int position = (alphLength - 1) - index;
+          cipher.append(alphabet.charAt(position));
+
+        } else if (d == b) {
+          int index = alphabet.indexOf(c);
+          int position = (alphLength - 1) - index;
+          cipher.append(alphaLower.charAt(position));
+        }
+
+      }
+
+    }
+
+    return cipher;
+
+  }
+
+  public static void atbashDeciphering() {
+
+    System.out.print("Enter encrypted message: ");
+    String cryptedString = keyboard.nextLine();
+
+    String alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    String alphaLower = "abcdefghijklmnopqrstuvwxyz";
+
+    int strLength = cryptedString.length();
+    int alphLength = alphabet.length();
+    StringBuilder cipher = new StringBuilder("");
+
+    for (int i = 0; i < strLength; i++) {
+      char b = cryptedString.charAt(i);
+      if (b == ' ') {
+        cipher.append(" ");
+      }
+
+      for (int j = 0; j < alphLength; j++) {
+        char c = alphabet.charAt(j);
+        char d = alphaLower.charAt(j);
+
+        if (c == b) {
+          int index = alphabet.indexOf(c);
+          int position = (alphLength - 1) - index;
+          cipher.append(alphabet.charAt(position));
+
+        } else if (d == b) {
+          int index = alphabet.indexOf(c);
+          int position = (alphLength - 1) - index;
+          cipher.append(alphaLower.charAt(position));
+        }
+
+      }
+
+    }
+    System.out.println(cipher);
+
+  }
+
+
+  public void encrypt(MouseEvent mouseEvent) {
+    String plainString = keyboard.nextLine();
+    lblOutput.setText(atbashCiphering(plainString).toString());
+
+
+  }
 }
