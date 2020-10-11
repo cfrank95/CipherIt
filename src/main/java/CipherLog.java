7446db81450fdae603995ab85d8fda2394ae41b0
Joseph Morelli <jgmorelli5207@eagle.fgcu.edu>
2020-10-11 08:41:15 -0400

    Record Log Testing

---
+++ src/main/java/CipherLog.java
@@ -0,0 +1,46 @@
+public class CipherLog {
+
+  int logNumber = 1;
+  String message = "";
+  String cipherType = "";
+
+  public CipherLog(String message, String cipherType) {
+    this.message = message;
+    this.cipherType = cipherType;
+
+  }
+  @Override
+  public String toString() {
+    String record =
+        "Log #: " + logNumber + " --- " + "Message Ciphered: " + message + " --- " + "Cipher Type: "
+            + cipherType + "\n";
+    logNumber++;
+    return record;
+  }
+
+  public int getLogNumber() {
+    return logNumber;
+  }
+
+  public void setLogNumber(int logNumber) {
+    this.logNumber = logNumber;
+  }
+
+  public String getMessage() {
+    return message;
+  }
+
+  public void setMessage(String message) {
+    this.message = message;
+  }
+
+  public String getCipherType() {
+    return cipherType;
+  }
+
+  public void setCipherType(String cipherType) {
+    this.cipherType = cipherType;
+  }
+
+
+}
